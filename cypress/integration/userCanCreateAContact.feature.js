5948cf48c79fc3b6f678cd331d99e8256c5f283c
psnoya <paulina.s.noya@gmail.com>
2020-08-20 20:51:45 +0200

    added js contact details

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -4,17 +4,17 @@ describe('user can create a contact', () => {
         cy.get('#add-contact').click()
         cy.get('#name').type('Noyaandstuff')
 		cy.get('#email').type('noyastuff@craft.se')
-		cy.get('#phone').type('0700 777799966')
+		cy.get('#phone').type('0700 606060')
 		cy.get('#company').type('Nope')
 		cy.get('#notes').type('Awesome nope')
         cy.get('#twitter').type('@god')
         cy.get('#submit').click()
     })
     it('displays a name of the new contact', () => {
-		cy.get('#contact-list').should('contain', 'Thomas')
+		cy.get('#contact-list').should('contain', 'Noyaandstuff')
 	})
 	
 	it('displays the phone number of the new contact', () => {
-		cy.get('#contact-list').should('contain', '0700 101010')
+		cy.get('#contact-list').should('contain', '0700 606060')
 	})
 })
