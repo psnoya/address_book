ae64a007e78a5b2c44cbec78306f7d7619903d7b
psnoya <paulina.s.noya@gmail.com>
2020-08-20 19:56:48 +0200

    added test for contact details

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -2,5 +2,11 @@ describe('user can create a contact', () => {
 	it('test', () => {
         cy.visit('http://localhost:3001')
         cy.get('#add-contact').click()
+        cy.get('#name').type('Noyaandstuff')
+		cy.get('#email').type('noyastuff@craft.se')
+		cy.get('#phone').type('0700 777799966')
+		cy.get('#company').type('Nope')
+		cy.get('#notes').type('Awesome nope')
+		cy.get('#twitter').type('@god')
 	})
 })
